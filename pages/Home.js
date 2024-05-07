827a699323cef42151a56011fc628fff376b71c4
ayush <ayushshaw016@gmail.com>
2024-05-07 12:48:03 +0530

    Updated the Pages Structure

--- pages/Home.js
+++ pages/Home.js
@@ -1,10 +1,12 @@
-import Navbar from "./Component/Navbar"
-import MainPage from "./Mainpage"
-import Footer from "./Component/Footer/Footer"
-export default function Home(){
-    return(<>
-<Navbar/>
-<MainPage/>
-<Footer/>
-    </>)
+import Navbar from "./Navbar";
+import MainPage from "./Mainpage";
+import Footer from "./Footer";
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <MainPage />
+      <Footer />
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Navbar from './Component/Navbar'
-import MainPage from './Mainpage'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Navbar from "./Navbar";
+import MainPage from "./Mainpage";
 
-import { useState } from 'react'
-import Footer from './Component/Footer/Footer'
-import Homepage from './Homepage'
-import Learnmore from './Learnmore'
+import { useState } from "react";
+import Footer from "./Footer";
+import Homepage from "./Homepage";
+import Learnmore from "./Learnmore";
 // import Carousel from './Carousel'
-import img from "../public/Assets/images/airpods.png"
-import img1 from "../public/Assets/images/frame.png"
-import Carousel from './Carousel'
+import img from "../public/Assets/images/airpods.png";
+import img1 from "../public/Assets/images/frame.png";
+import Carousel from "./Carousel";
 export default function Home() {
-  const images=[
-    img,
-    img1
-  ]
+  const images = [img, img1];
 
-  
-  
   return (
     <>
       <Navbar />
       {/* <Learnmore/> */}
       <Homepage />
       {/* <Carousel images={images}/> */}
-      
 
       <Footer />
     </>
-  )
+  );
 }
